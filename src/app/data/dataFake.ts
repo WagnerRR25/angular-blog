3bf1422269705fa3b29c5e6dce04a83ae14ee2ee
WagnerRR25 <rodrigueswagner09@gmail.com>
2023-12-29 00:26:55 -0300

    feat:  changes to my blog my way

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -3,12 +3,12 @@ export const dataFake = [
     "id":"1",
     "title": "NOVO HOMEM DE FERRO EM 3D",
     "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "photoCover":"https://as2.ftcdn.net/v2/jpg/02/67/16/97/1000_F_267169775_UmCM5mkFGGufp64FclXg1XJXD7oKjp24.jpg"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "Florianópolis terá festas privadas de luxo na virada do ano; programe-se",
+    "description": "Florianópolis terá uma variedade de festas privadas para celebrar a virada do ano, muitas com padrão luxo. Os ingressos variam de preços, mas em beach club de Jurerê Internacional já passam de R$ 3 mil para homens. Os valores ainda podem aumentar à medida em que o fim do ano se aproxima.",
+    "photoCover":"https://s2.glbimg.com/BcXNSRXLkmdic7yNH1tlUsdUtKg=/620x465/s.glbimg.com/jo/g1/f/original/2014/12/30/foto_reveillon_em_florianopolis_credito_cristiano_estrela_1.jpg"
   }
 ]
