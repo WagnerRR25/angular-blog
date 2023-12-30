50cad3ab0a7df0688e56a98f54186d8e75155fd5
WagnerRR25 <rodrigueswagner09@gmail.com>
2023-12-29 23:09:30 -0300

    feat:  Changes to style, descriptions and images

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://as2.ftcdn.net/v2/jpg/02/67/16/97/1000_F_267169775_UmCM5mkFGGufp64FclXg1XJXD7oKjp24.jpg"
+    "title": "Réveillon 2024: o espetáculo de drones no céu de Florianópolis",
+    "description": "Após anos de tradição, Florianópolis terá pela primeira vez um Réveillon sem a queima de fogos de artifício, mas prefeitura garante efeitos visuais com show de drones, com luzes, arte e tecnologia",
+    "photoCover":"https://wp.pt.aleteia.org/wp-content/uploads/sites/5/2023/12/Sky-Elements-record-breaking-Christmas-light-show.jpg?w=620&h=348&crop=1"
   },
   {
     "id":"2",
     "title": "Florianópolis terá festas privadas de luxo na virada do ano; programe-se",
     "description": "Florianópolis terá uma variedade de festas privadas para celebrar a virada do ano, muitas com padrão luxo. Os ingressos variam de preços, mas em beach club de Jurerê Internacional já passam de R$ 3 mil para homens. Os valores ainda podem aumentar à medida em que o fim do ano se aproxima.",
-    "photoCover":"https://s2.glbimg.com/BcXNSRXLkmdic7yNH1tlUsdUtKg=/620x465/s.glbimg.com/jo/g1/f/original/2014/12/30/foto_reveillon_em_florianopolis_credito_cristiano_estrela_1.jpg"
+    "photoCover":"https://portaldailha.com.br/noticias/fotos/beach-club-jurere.jpg"
+  },
+  {
+    "id":"3",
+    "title": "Tradicionais fogos de artifício serão substituídos",
+    "description": "O Réveillon 2024 em Florianópolis promete ser inédito e pioneiro no Brasil. Isso porque, pela primeira vez, os tradicionais fogos de artifício serão substituídos por um show de drones, integrando um espetáculo de luzes, arte e tecnologia, além de uma série de atrações regionais e nacionais na Beira-Mar Norte e Continental.",
+    "photoCover":"https://static.ndmais.com.br/2020/01/WhatsApp-Image-2020-01-01-at-8.48.16-AM-800x533.jpeg"
+  },
+  {
+    "id":"4",
+    "title": "Na trilha Floripa",
+    "description": "Caminhar enquanto admira as paisagens naturais, respirando ar puro. Quem ama essa sensação, vai se apaixonar pelas várias opções de trilhas em Florianópolis. Se você está de férias na cidade, poderá escolher desde os caminhos mais leves até os mais longos e aproveitar a sua viagem. Por isso, listamos algumas das trilhas mais encantadoras para você desbravar em Floripa.",
+    "photoCover":"https://www.viajali.com.br/wp-content/uploads/2020/03/trilhas-florianopolis-1.jpg"
   }
 ]
